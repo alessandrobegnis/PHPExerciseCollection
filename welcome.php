cd3bf33b8723669aab4c8982a05b48381861993f
Alessandro <begnis.alessandro@studenti.salesianisesto.it>
2020-11-13 10:14:01 +0100

    add write login infos in a file

--- welcome.php
+++ welcome.php
@@ -1,18 +1,32 @@
 <html>
-    <body>
-        Benvenuto <?php 
 
-        include("function.php");
+<body>
+    Benvenuto <?php
 
-        echo $_POST["name"] . " " . $_POST["lastname"] . "<br/>" . 
-        "Data Di Nascita: " . $_POST["data"] . "<br/>" . 
-        "Sesso: " . $_POST["sesso"] . "<br/>" . 
-        "Descrizione: " . $_POST["descrizione"] . "<br/>"; 
 
-        debug_to_console($_POST);
+                $file = "ciao.txt";
+                $handler = fopen($file, "a+") or  die("File not exists");
 
-		echo "<br/>";
-		
-        ?>
-    </body>
+                if (isset($_POST["name"]) && isset($_POST["lastname"]) && isset($_POST["sesso"]) && isset($_POST["descrizione"]) && isset($_POST["data"])) {
+                    fwrite($handler, "\n" . $_POST["name"]);
+                    fwrite($handler, "\n" . $_POST["lastname"]);
+                    fwrite($handler, "\n" . $_POST["sesso"]);
+                    fwrite($handler, "\n" . $_POST["descrizione"]);
+                    fwrite($handler, "\n" . $_POST["data"]);
+                }
+
+                include("function.php");
+
+                echo $_POST["name"] . " " . $_POST["lastname"] . "<br/>" .
+                    "Data Di Nascita: " . $_POST["data"] . "<br/>" .
+                    "Sesso: " . $_POST["sesso"] . "<br/>" .
+                    "Descrizione: " . $_POST["descrizione"] . "<br/>";
+
+                debug_to_console($_POST);
+
+                echo "<br/>";
+
+                ?>
+</body>
+
 </html>
